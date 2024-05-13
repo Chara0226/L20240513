28db8216598599c0c8bb0efe7961d7a7431109b1
Chara <sscc216@naver.com>
2024-05-13 09:37:10 +0900

    initialize File

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+#include	<iostream>
+
+using	namespace std;
+
+int	main()
+{
+	int Data = 0;
+	int Data2 = 0;
+
+	std::cin >> Data;
+	std::cin >> Data2;
+
+	std::cout << Data + Data2 << std::endl;
+	std::cout << Data - Data2 << std::endl;
+	std::cout << Data / Data2 << std::endl;
+	std::cout << Data * Data2 << std::endl;
+
+	return 0;
+
+}
